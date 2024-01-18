95240a1f118cb89344a13778f121f19f03955b00
jay kumar gupta <jaynrk2002@gmail.com>
2024-01-18 11:35:36 +0530

    second commit adding my frontend(jay)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from "react";
 import Navcomp from "./components/Navbar";
 import Footercomp from "./components/Footer";
+import {  
+  BrowserRouter as Router,  
+  Routes,  
+  Route,  
+  Link  
+}   
+from 'react-router-dom';  
 
 
 function App() {
